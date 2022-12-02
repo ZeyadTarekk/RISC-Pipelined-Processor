06862a1b45c7a7bd5d3fa38a5e46fe43b0f6d2e5
Yousef Al-waer <elwaeryousef@gmail.com>
2022-12-02 21:27:34 +0200

    adding the HazardDetectionUnit

---
+++ Project/HazardDetectionUnit.v
@@ -0,0 +1,6 @@
+module HazardDetectionUnit (
+    input MemRead,
+    input [3:0] ALUDest ,  DecodeSrc , DecodeDest,
+    output reg MakeMeBubble);
+    assign MakeMeBubble = MemRead && (ALUDest == DecodeSrc || ALUDest == DecodeDest);
+endmodule
